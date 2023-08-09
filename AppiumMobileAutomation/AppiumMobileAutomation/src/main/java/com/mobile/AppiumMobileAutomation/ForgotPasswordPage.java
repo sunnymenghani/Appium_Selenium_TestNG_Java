0eb6cc210a019bdc5cff5fe0d053fe6ddf76cd61
sunnymenghani <sunnymenghani@users.noreply.github.com>
2023-08-09 14:30:44 +0530

    Add files via upload

---
+++ AppiumMobileAutomation/AppiumMobileAutomation/src/main/java/com/mobile/AppiumMobileAutomation/ForgotPasswordPage.java
@@ -0,0 +1,77 @@
+package com.mobile.AppiumMobileAutomation;
+
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+
+public class ForgotPasswordPage {
+
+	private AppiumDriver driver;
+    private WebDriverWait wait;
+
+    @AndroidFindBy(id = "com.example.app:id/emailEditText")
+    public MobileElement enterEmailToRecoverPassword;
+    
+    @AndroidFindBy(id = "com.example.app:id/recoverPasswordButton")
+    public MobileElement recoverPasswordButton;
+    
+    @AndroidFindBy(id = "com.example.app:id/loginFailMessage")
+    public MobileElement resetPasswordMailSentMessage;
+    
+    @AndroidFindBy(id = "com.example.app:id/otpEditText")
+    public MobileElement enterOTP;
+    
+    @AndroidFindBy(id = "com.example.app:id/submitOtpButton")
+    public MobileElement submitOTP;
+    
+    @AndroidFindBy(id = "com.example.app:id/newPasswordEditText")
+    public MobileElement enterNewPasswordElement;
+    
+    @AndroidFindBy(id = "com.example.app:id/submitNewPasswordButton")
+    public MobileElement setNewPasswordElement;
+    
+    public ForgotPasswordPage(AppiumDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 10);
+        PageFactory.initElements(driver, this);
+    }
+    
+    public MobileElement resetPasswordScreen() {
+    	return enterEmailToRecoverPassword;
+    }
+    
+    public MobileElement enterEmailToResetPassword(String email) {
+    	wait.until(ExpectedConditions.visibilityOf(enterEmailToRecoverPassword));
+    	enterEmailToRecoverPassword.sendKeys(email);
+    	return enterEmailToRecoverPassword;
+    }
+    
+    public MobileElement enterOtpToResetPassword(String otp) {
+    	wait.until(ExpectedConditions.visibilityOf(enterOTP));
+    	enterOTP.sendKeys(otp);
+    	return enterOTP;
+    }
+    
+    public MobileElement submitOtpButton() {
+    	wait.until(ExpectedConditions.visibilityOf(submitOTP));
+    	submitOTP.click();
+    	return submitOTP;
+    }
+
+    public MobileElement enterNewPassword(String newPassword) {
+    	wait.until(ExpectedConditions.visibilityOf(enterNewPasswordElement));
+    	enterNewPasswordElement.sendKeys(newPassword);
+    	return enterNewPasswordElement;
+    }
+    
+    public MobileElement submitNewPasswordButton() {
+    	wait.until(ExpectedConditions.visibilityOf(setNewPasswordElement));
+    	setNewPasswordElement.click();
+    	return setNewPasswordElement;
+    }
+    
+}
